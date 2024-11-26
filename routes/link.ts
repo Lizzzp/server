a964a5ac1246b5650f642b2059c5fcbf3fded706
Lizzzp <858247021@qq.com>
2024-11-26 17:41:53 +0800

    完成链接标记的接口,待测试

--- routes/link.ts
+++ routes/link.ts
@@ -1,30 +1,31 @@
-import { Request, Response, Router } from "express";
-import sequelize, { oaDB } from "../db/dbConnect";
+import e, { Request, Response, Router } from "express";
+import  { MeianDB, oaDB } from "../db/dbConnect";
 import { QueryTypes } from "sequelize";
 const router = Router();
 
-
-router.post("/", async (req: Request, res: Response) => {
-  req.setTimeout(300 * 1000);
+router.post("/query", async (req: Request, res: Response) => {
+  req.setTimeout(300 * 1000); // 查询过慢
   const {
-    shop,
-    conversion_rate_l,
-    conversion_rate_h,
-    sales_numbers,
-    linkids,
-    begin_date,
-    end_date,
-    promotion_intensity,
-    sales_rank,
-    sales_volume,
-    profit_rate_l,
-    profit_rate_h,
-    p_r_l,
-    p_r_h
+    shop, // 店铺
+    conversion_rate_l, //转换率
+    conversion_rate_h, 
+    sales_numbers, // 销量
+    linkids,        // 链接IDs
+    begin_date,     // 开始时间
+    end_date,       // 结束时间
+    promotion_intensity, // 推广力度
+    sales_rank, // 热销等级
+    sales_volume, // 销售额
+    profit_rate_l, // 利润率
+    profit_rate_h, //
+    p_r_l,  // 推广费占比
+    p_r_h,
+    head
   } = req.body;
 
   let sqlWhereOne = "";
-  if (shop!==undefined) {
+  if (shop) {
+    sqlWhereOne += sqlWhereOne.length ? " and " : " WHERE ";
     sqlWhereOne += `[店铺]='${shop}' `;
   }
 
@@ -36,25 +37,14 @@ router.post("/", async (req: Request, res: Response) => {
     sqlWhereOne += sqlWhereOne.length ? " and " : " WHERE ";
     sqlWhereOne += `[业务日期]<='${end_date}' `;
   }
+  // console.log(linkids);
+  // if (linkids!==undefined) {
+  //   sqlWhereOne += sqlWhereOne.length ? " and " : " WHERE ";
+  //   sqlWhereOne += `[链接ID] IN (\'${(linkids as string[]).join("','")}\')`;
+  // }
 
-  if (linkids!==undefined) {
-    sqlWhereOne += sqlWhereOne.length ? " and " : " WHERE ";
-    sqlWhereOne += `[链接ID] IN (\'${(linkids as string[]).join("','")}\')`;
-  }
 
-  let sqlWhereTwo = "";
 
-  // 销售额
-  if (sales_volume!==undefined) {
-    sqlWhereTwo += sqlWhereTwo.length ? " and " : " HAVING ";
-    sqlWhereTwo += `SUM([销售额]) >= ${sales_volume} `;
-  }
-
-  // 销售件数
-  if (sales_numbers!==undefined) {
-    sqlWhereTwo += sqlWhereTwo.length ? " and " : " HAVING ";
-    sqlWhereTwo += `SUM([销售件数]) >= ${sales_numbers} `;
-  }
   let sqlWhereThree = "";
 
   // 转化率
@@ -86,13 +76,36 @@ router.post("/", async (req: Request, res: Response) => {
     sqlWhereThree += sqlWhereThree.length ? " and " : " WHERE ";
     sqlWhereThree += `a.[推广费占比] <= ${p_r_h} `;
   }
+  // 销售额
+  if (sales_volume!==undefined) {
+    sqlWhereThree += sqlWhereThree.length ? " and " : " WHERE ";
+    sqlWhereThree += `a.[销售额] >= ${sales_volume} `;
+  }
 
-  const links1 = await sequelize.query(
-    `
-        SELECT * 
-        FROM (
-          SELECT
-          SUM([销售件数]) AS 销售件数,
+  // 销售件数
+  if (sales_numbers!==undefined) {
+    sqlWhereThree += sqlWhereThree.length ? " and " : " WHERE ";
+    sqlWhereThree += `a.[销售件数] >= ${sales_numbers} `;
+  }
+
+  try {
+    const links1 = await MeianDB.query(`
+SELECT
+  c.* ,
+  b.[销售等级] AS sales_rank,
+  b.[负责人] AS head,
+  b.[推款阶段] AS promotion_intensity,
+  b.[主图],
+  b.[店铺名称],
+  b.[创建时间],
+  b.[链接URL] 
+FROM
+  (
+    SELECT
+      * 
+    FROM
+      (
+        SELECT
           [链接ID],
           SUM([销售额]) AS [销售额],
           SUM([访客数]) AS 访客数,
@@ -100,86 +113,87 @@ router.post("/", async (req: Request, res: Response) => {
           SUM([平台广告费] + [刷单广告费] + [联盟广告费]) AS 推广费,
           SUM([销售利润]) AS 销售利润,
         CASE
+            
+            WHEN SUM([销售件数]) IS NULL THEN
+            0 ELSE SUM([销售件数]) 
+          END AS 销售件数,
+        CASE
+            
             WHEN SUM([访客数]) = 0 THEN
-            NULL ELSE SUM([买家数]) / SUM([访客数]) 
+            0 ELSE SUM([买家数]) / SUM([访客数]) 
           END AS 转化率,
         CASE
+            
             WHEN SUM([销售额]) = 0 THEN
-            NULL ELSE SUM([平台广告费] + [刷单广告费] + [联盟广告费]) / SUM([销售额]) 
+            0 ELSE SUM([平台广告费] + [刷单广告费] + [联盟广告费]) / SUM([销售额]) 
           END AS 推广费占比,
         CASE
             
             WHEN SUM([销售额]) = 0 THEN
-            NULL ELSE SUM([销售利润]) / SUM([销售额]) 
+            0 ELSE SUM([销售利润]) / SUM([销售额]) 
           END AS 利润率 
         FROM
           [DW_Links].[链接日报] 
-    ${sqlWhereOne}
-        GROUP BY [链接ID] 
-    ${sqlWhereTwo}
-        ) AS a
-            ${sqlWhereThree}
-                `,
-    {
-      type: QueryTypes.SELECT,
-    }
-  );
-
-  if (links1.length) {
-    const linkids = links1.map((l: any) => l["链接ID"]);
-    let links2: any[] = [];
-    for (let index = 0; index < linkids.length; index += 10000) {
-      const d = await sequelize.query(
-        `SELECT * FROM [DIM].[链接表] WHERE [链接ID] IN (\'${linkids
-          .slice(index, index + 10000)
-          .join("','")}\')`,
+        ${sqlWhereOne}
+        GROUP BY
+      [链接ID]) AS a 
+    ${sqlWhereThree}
+  AND a.[销售件数] >= 0) AS c
+  LEFT JOIN [DIM].[链接表] AS b ON c.[链接ID] = b.[链接ID]`,
         {
           type: QueryTypes.SELECT,
         }
       );
-      links2 = links2.concat(d);
-    }
-
-    const links3 = await oaDB.query(
-      `
-            SELECT * 
-            FROM link_mark`,
-      {
-        type: QueryTypes.SELECT,
+    
+      if (links1.length) {
+        const links3 = await oaDB.query(
+          `SELECT * FROM link_mark`,
+          {
+            type: QueryTypes.SELECT,
+          }
+        );
+        const data = links1
+          .map((l: any) => {
+            const target2 = links3.find((k: any) => k["link_id"] === l["链接ID"]);
+            let link = { ...l };
+            if (target2) {
+              link = { ...link, ...target2 };
+            }
+            return link;
+          })
+          .filter((l) => {
+            let flag = true;
+            if (promotion_intensity) {
+              flag = flag && l.promotion_intensity === promotion_intensity;
+            }
+            if (sales_rank) {
+              flag = flag && l.sales_rank === sales_rank;
+            }
+            if(head) {
+              flag = flag && l.head === head;
+            }
+            if (linkids) {
+              if (linkids instanceof Array) {
+                flag = flag && linkids.includes(l['链接ID'])
+              } else if (typeof linkids === 'string') {
+                flag = flag && linkids === l['链接ID']
+              }
+            }
+            return flag;
+          });
+        res.status(200).json({
+          data
+        });
+      } else {
+        res.status(200).json({
+          data: [],
+        });
       }
-    );
-    const data = links1
-      .map((l: any) => {
-        const target1 = links2.find((k: any) => k["链接ID"] === l["链接ID"]);
-        const target2 = links3.find((k: any) => k["link_id"] === l["链接ID"]);
-        let link = { ...l };
-        if (target1) {
-          link = { ...link, ...target1 };
-        }
-        if (target2) {
-          link = { ...link, ...target2 };
-        }
-        return link;
-      })
-      .filter((l) => {
-        let flag = true;
-        if (promotion_intensity) {
-          flag = flag && l.promotion_intensity === promotion_intensity;
-        }
-        if (sales_rank) {
-          flag = flag && l.sales_rank === sales_rank;
-        }
-        return flag;
-      });
+  } catch (error) {
+    console.log(error);
     res.status(200).json({
-      data: data.slice(0, 100),
-    });
-  } else {
-    res.status(200).json({
-      data: [],
+        data: [],
     });
   }
 });
-
-
 export default router;
